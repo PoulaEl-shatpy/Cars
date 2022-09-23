2a449a3ff5e56d1f44ab7d7c779f7a3e88980dbb
PoulaEl-shatpy <98977441+PoulaEl-shatpy@users.noreply.github.com>
2022-09-23 15:29:55 +0200

    bookingPod middle container

--- Main-folder/src/components/home/top-area/booking-Pod/middle-container/Booking-pod-middle.js
+++ Main-folder/src/components/home/top-area/booking-Pod/middle-container/Booking-pod-middle.js
@@ -1,28 +1,99 @@
 import React from "react";
 import "./Booking-pod-middle.css"
-
+import { useState } from "react";
 
 
 
 export default function BookingPodMiddle() {
+
+    const [form, setForm] = useState({
+        sameReturn: false,
+        pickupLocation: "",
+        returnLocation: ""
+    });
+
+    const handleChange = (event) => {
+        const {name, value, type, checked} = event.target
+        setForm(prevState => {
+            return {
+                ...prevState,
+                [name]: type === "checkbox" ? checked : value
+            }
+        })
+    }
+
+    const style = {
+        fakeInputBackgroundColor: form.sameReturn ? "#fff" : "#090",
+        returnLocationDisplay: form.sameReturn ? "inline-block" : "none",
+        pickupLocationWidth: form.sameReturn ? "100%" : "200%",
+        // placeholder: form.sameReturn ? "Pick up location" : "Pick up & Return location"
+    }
+    
     return (
-        <div className="bookig-bod__middle-container">
-            <div className="station-dataAndTime">
-                <div className="search-input">
-                    <div className="search-header">
-                        <p className="search-title__text">Pick up & Return location</p>
-                        <div className="Check__Box-container">
-                            <div className="check__box">
-                                <input type="checkbox" id="sameReturn" className="checkbox" value="sameReturn" />
-                                <label htmlFor="sameReturn" className="check__box-label">
-                                    <span className="fake-input"></span>
-                                    <p className="label-title">Same return location</p>
-                                </label>
-                            </div>
-                        </div>
+        <div className="booking-pod-middle-container">
+
+            <div className="pickup-return-container">
+
+                <div className="pickup-header">
+
+                    <div className="header-text__container">
+                    <p className="header-text">Pick up & Return location</p>
                     </div>
+
+                    <div className="checkbox-container">
+                        <input
+                            id="checkbox"
+                            className="checkbox"
+                            type="checkbox"
+                            name="sameReturn"
+                            value={form.sameReturn}
+                            onChange={handleChange}
+                        ></input>
+                        <label htmlFor="checkbox">
+                            <sapn className="fake-input" style={{backgroundColor: style.fakeInputBackgroundColor}}></sapn>
+                            Same Return location
+                        </label>
+                    </div>
+
+                </div>
+
+                <div className="inputs-field">
+                    <div className="pickup__location-input">
+                        <span className="material-symbols-outlined icon-location">location_on</span>
+                        <input
+                            placeholder={form.sameReturn ? "Pick up location" : "Pick up & Return location"}
+                            className="pickup-location"
+                            type="text"
+                            name="pickupLocation"
+                            value={form.pickupLocation}
+                            onChange={handleChange}
+                            style={{width: style.pickupLocationWidth, placeholder: style.placeholder}}
+                            />
+                    </div>
+                    {/* //////////////////////////////////////////////// */}
+                    <div className="return__location-input" style={{display: style.returnLocationDisplay}}>
+                    <span className="material-symbols-outlined icon-location">location_on</span>
+                    <input
+                        placeholder="Return location"
+                        className="return-location"
+                        type="text"
+                        name="returnLocation"
+                        value={form.returnLocation}
+                        onChange={handleChange}
+                        
+                    />
+                    </div>
+
                 </div>
             </div>
+            <div className="datetime"></div>
         </div>
     )
+
 }
+
+
+
+
+
+
