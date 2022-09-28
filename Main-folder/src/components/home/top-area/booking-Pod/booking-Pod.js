f16577bef3259f9a2d653ffc40c40a24a1a3b375
PoulaEl-shatpy <98977441+PoulaEl-shatpy@users.noreply.github.com>
2022-09-28 22:08:36 +0200

    booking pod bottom

--- Main-folder/src/components/home/top-area/booking-Pod/booking-Pod.js
+++ Main-folder/src/components/home/top-area/booking-Pod/booking-Pod.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./booking-Pod.css";
 import BookingPodTop from "./top-container/Booking-pod-top";
 import BookingPodMiddle from "./middle-container/Booking-pod-middle";
+import BookingPodBottom from "./bookingPod-bottom-container/booking-pod-bottom";
 
 
 export default function BookingBod() {
@@ -10,6 +11,7 @@ export default function BookingBod() {
             <div className="hero__taplet">
                 <BookingPodTop />
                 <BookingPodMiddle />
+                <BookingPodBottom />
                 <div style={{ width: '100px', height: '100px' }}></div>
             </div>
         </div>
---
+++ Main-folder/src/components/home/top-area/booking-Pod/bookingPod-bottom-container/booking-pod-bottom.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Bookin-pod-bottm.css"
+
+
+
+export default function BookingPodBottom() {
+
+
+    return (
+        
+    )
+}
